084addb015db29e35d7847d146100754aeaf91de
ulia-lukash <ulianalukash@gmail.com>
2025-12-06 17:52:01 +0300

    navbar base

--- app/landing-page/landingPage.tsx
+++ app/landing-page/landingPage.tsx
@@ -1,6 +1,9 @@
+import { Header } from "./components/header"
+
 export function LandingPage() {
     return (
-        <main>
+        <main className="main-page">
+            <Header/>
             <p>AAAAAAA</p>
         </main>
     )
