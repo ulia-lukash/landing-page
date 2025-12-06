b627ba28d03457aa68df29340c4ab485aebadac5
ulia-lukash <ulianalukash@gmail.com>
2025-12-06 21:35:23 +0300

    tablet screen sizes for not image components

--- app/landing-page/components/logoCloud.tsx
+++ app/landing-page/components/logoCloud.tsx
@@ -3,12 +3,25 @@ export function LogoCloud() {
         <div className="logo-cloud-container">
             <div className="label">Trusted by:</div>
             <div className="logo-row">
-                <img src="Logo.svg" alt="" className="logo-img" />
-                <img src="Logo-1.svg" alt="" className="logo-img" />
-                <img src="Logo-2.svg" alt="" className="logo-img" />
-                <img src="Logo-3.svg" alt="" className="logo-img" />
-                <img src="Logo-4.svg" alt="" className="logo-img" />
-                <img src="Logo-5.svg" alt="" className="logo-img" />
+                <div className="logo-img">
+                    <img src="Logo.svg" alt="" />
+                </div>
+                <div className="logo-img">
+                    <img src="Logo-1.svg" alt="" />
+                </div>
+                <div className="logo-img">
+                    <img src="Logo-2.svg" alt="" />
+                </div>
+                <div className="logo-img">
+                    <img src="Logo-3.svg" alt="" />
+                </div>
+                <div className="logo-img">
+                    <img src="Logo-4.svg" alt="" />
+                </div>
+                <div className="logo-img">
+                    <img src="Logo-5.svg" alt="" />
+                </div>
+                
             </div>
         </div>
     )
